543b4654745dc918ed2581a26f9abb76adda4dd8
lollomrt <lorenzomartini.mrt@outlook.it>
2022-11-29 17:03:26 +0100

    ripush

--- js/javascript.js
+++ js/javascript.js
@@ -1,28 +1,23 @@
 // 1. individuare le variabii del ciclo: partenza 1, fine 100 e incrementale 1++.
 for (let i = 1; i <= 100; i++) {
     // 2. Controllo: creare all'interno del ciclo le condizioni if else
-    let Fizz
-    let Buzz
-    let FizzBuzz
+    let word
 
     if (i % 3 == 0){
-        Fizz = "Fizz"    
+        word = "Fizz"    
     }
     else if (i % 5 == 0) {
-        Buzz = "Buzz"   
+        word = "Buzz"   
     }
 
     else if (i % 3 == 0 && i % 5 == 0) {
-        FizzBuzz = "FizzBuzz"    
+        word = "FizzBuzz"    
     }
 
     else {
-        i = i    
+        word = i    
     }
 
-    console.log(i)
-    console.log(Fizz)
-    console.log(Buzz)
-    console.log(FizzBuzz)
+    console.log(word)
 }
 
