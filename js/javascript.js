19e4edcf22f73f9d376aa66d50f131fbe6fa84e7
lollomrt <lorenzomartini.mrt@outlook.it>
2022-11-29 16:58:00 +0100

    Revisone Alessio

--- js/javascript.js
+++ js/javascript.js
@@ -1,26 +1,28 @@
 // 1. individuare le variabii del ciclo: partenza 1, fine 100 e incrementale 1++.
 for (let i = 1; i <= 100; i++) {
+    // 2. Controllo: creare all'interno del ciclo le condizioni if else
+    let Fizz
+    let Buzz
+    let FizzBuzz
+
     if (i % 3 == 0){
-        i = "Fizz"
-        
+        Fizz = "Fizz"    
     }
     else if (i % 5 == 0) {
-        i = "Buzz"
-        
+        Buzz = "Buzz"   
     }
 
     else if (i % 3 == 0 && i % 5 == 0) {
-        i = "FizzBuzz"
-        
+        FizzBuzz = "FizzBuzz"    
     }
 
     else {
-        i = i
-        
+        i = i    
     }
 
     console.log(i)
+    console.log(Fizz)
+    console.log(Buzz)
+    console.log(FizzBuzz)
 }
-
-// 2. Controllo: creare all'interno del ciclo le condizioni if else
 
---
+++ readme.txt
@@ -0,0 +1,8 @@
+Scrivi un programma che stampi in console i numeri da 1 a 100, ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”. Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+
+1. individuare le variabii del ciclo: partenza 1, fine 100 e incrementale 1++.
+2. Controllo: creare all'interno del ciclo le condizioni if else
+    2.1 se numero è divisibile per 3 --> display Fizz 
+    2.2 se numero è divisibile per 5 --> display Buzz 
+    2.3 se numero divisibile per 3 e 5 --> display FizzBuzz 
+    2.4 altrimenti display numero
