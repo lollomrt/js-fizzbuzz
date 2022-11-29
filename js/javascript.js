eea5d0f658a00eee3c2ab7b5d8bf94212c0de576
lollomrt <lorenzomartini.mrt@outlook.it>
2022-11-29 17:12:56 +0100

    Esercizio finito - inizio bonus

--- js/javascript.js
+++ js/javascript.js
@@ -3,15 +3,15 @@ for (let i = 1; i <= 100; i++) {
     // 2. Controllo: creare all'interno del ciclo le condizioni if else
     let word
 
-    if (i % 3 == 0){
-        word = "Fizz"    
+    if (i % 3 == 0 && i % 5 == 0){ 
+        word = "FizzBuzz"    
     }
-    else if (i % 5 == 0) {
-        word = "Buzz"   
+    else if (i % 3 == 0) {
+        word = "Fizz"   
     }
 
-    else if (i % 3 == 0 && i % 5 == 0) {
-        word = "FizzBuzz"    
+    else if (i % 5 == 0) {
+        word = "Buzz"    
     }
 
     else {
