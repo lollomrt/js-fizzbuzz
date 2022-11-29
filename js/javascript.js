214522856f55034c6b02548ba02df02e092ebf14
lollomrt <lorenzomartini.mrt@outlook.it>
2022-11-29 18:59:43 +0100

    Fine Bonus

--- js/javascript.js
+++ js/javascript.js
@@ -1,21 +1,32 @@
 // 1. individuare le variabii del ciclo: partenza 1, fine 100 e incrementale 1++.
+const display = document.getElementById("container")
+
 for (let i = 1; i <= 100; i++) {
     // 2. Controllo: creare all'interno del ciclo le condizioni if else
     let word
+    let element
 
     if (i % 3 == 0 && i % 5 == 0){ 
-        word = "FizzBuzz"    
+        word = "FizzBuzz"
+        element = `<div class="quadrato fizzbuzz">${word}</div>`
+        display.innerHTML += element
     }
     else if (i % 3 == 0) {
-        word = "Fizz"   
+        word = "Fizz"
+        element = `<div class="quadrato fizz">${word}</div>`
+        display.innerHTML += element  
     }
 
     else if (i % 5 == 0) {
-        word = "Buzz"    
+        word = "Buzz"
+        element = `<div class="quadrato buzz">${word}</div>`
+        display.innerHTML += element    
     }
 
     else {
-        word = i    
+        word = i
+        element = `<div class="quadrato">${i}</div>`
+        display.innerHTML += element   
     }
 
     console.log(word)
